4f293b9fbdbae7b0937e712cc7b68b28b18477c4
Radoslaw Osten-Sacken <inf106520@polluks.cs.put.poznan.pl>
2014-06-17 11:45:46 +0200

    poprawki

--- kod.cpp
+++ kod.cpp
@@ -26,8 +26,8 @@ int getRoom();
 int main(int argc, char **argv)
 {
 	int mastersNum, roomsNum;
-	mastersNum = 10;
-	roomsNum = 10;
+	mastersNum = 5;
+	roomsNum = 5;
 	vector< set<P, greater<P> > > mastersQ(mastersNum, set<P, greater<P> >());
 	vector< set<P, greater<P> > > roomsQ(roomsNum, set<P, greater<P> >());
 
@@ -63,7 +63,8 @@ int main(int argc, char **argv)
 	int msg4[4] = {0};	// <zasób><timestamp><moc><version>
 	int msg[4] = {0};	// <zasób><timestamp><moc><version>
 		
-
+	int min_size, min_index;
+	
 	struct timeval tv;
 
 	srand(time(NULL)+tid);
@@ -71,11 +72,16 @@ int main(int argc, char **argv)
 		sleep(1);	
 		
 		switch(stan){
-			case vacant:
-				//myMaster = getMaster();	// dodaje do własnej kolejki i wysyła żądania do wszystkich innych
-// TUTAJ->>>	//stan=gettingMaster;
-				//myRoom = getRoom(); // dodaje do własnej kolejki i wysyła żądania do wszystkich innych
-				myMaster = rand()%mastersNum;
+			case vacant:				
+				min_size=100000;
+				min_index=0;
+				for(int i=0;i<mastersNum;i++)
+					if(mastersQ[i].size()<min_size) {
+						min_size = mastersQ[i].size();
+						min_index = i;
+					}
+				myMaster=min_index;				
+				
 				mastersQ[myMaster].insert(P(lecturesDone, tid));
 				gettimeofday(&tv, NULL);
 				cout<<tv.tv_sec*1000000+tv.tv_usec<<" # "<<tid<<": waiting for MyMaster: "<<myMaster<<endl;
@@ -92,8 +98,16 @@ int main(int argc, char **argv)
 			case gettingMaster:
 				if(responses.size() == size-1) {
 					responses.clear();
-					//myRoom = getRoom(); // dodaje do własnej kolejki i wysyła żądania do wszystkich innych
-					myRoom = rand()%roomsNum;
+					
+					min_size=100000;
+					min_index=0;
+					for(int i=0;i<roomsNum;i++)
+						if(roomsQ[i].size()<min_size) {
+							min_size = roomsQ[i].size();
+							min_index = i;
+						}
+					myRoom=min_index;				
+
 					roomsQ[myRoom].insert(P(lecturesDone, tid));
 					gettimeofday(&tv, NULL);
 					cout<<tv.tv_sec*1000000+tv.tv_usec<<" # "<<tid<<": waiting for MyRoom: "<<myRoom<<endl;
@@ -117,10 +131,6 @@ int main(int argc, char **argv)
 					gettimeofday(&tv, NULL);
 					cout<<tv.tv_sec*1000000+tv.tv_usec<<" # "<<tid<<": lecture START master: " << myMaster << "  room:"<<myRoom<<endl;
 				}
-			/*	
-				else
-					cout<<"responses: "<<responses.size()<<endl;
-			*/
 				break;
 			case lecture:
 				if(time(NULL)>=lectureEnd) {
@@ -131,8 +141,11 @@ int main(int argc, char **argv)
 					msg[1]=lecturesDone;
 					myRoom=-1;
 					for(int i=0; i<size; i++)
-						if(i!=tid)
+						if(i!=tid) {
 							MPI_Send(&msg, 4, MPI_INT, i, RELEASE_ROOM_TAG, MPI_COMM_WORLD );
+							gettimeofday(&tv, NULL);
+							cout<<tv.tv_sec*1000000+tv.tv_usec<<" # "<<tid<<": send RELESAE_ROOM_TAG  to:"<<i<<endl;
+						}
 					lecturesDone++;
 					mastersPower[myMaster].first--;
 					if(mastersPower[myMaster].first > 0) {
@@ -144,8 +157,11 @@ int main(int argc, char **argv)
 						msg[3]=mastersPower[myMaster].second;
 						myMaster=-1;
 						for(int i=0; i<size; i++)
-							if(i!=tid)
+							if(i!=tid){
 								MPI_Send(&msg, 4, MPI_INT, i, RELEASE_MASTER_TAG, MPI_COMM_WORLD );
+								gettimeofday(&tv, NULL);
+								cout<<tv.tv_sec*1000000+tv.tv_usec<<" # "<<tid<<": send RELEASE_MASTER_TAG to: " << i << "  (power " <<msg[2]<<" v"<< msg[3]<< ")"<<endl;
+							}
 						stan=vacant;
 					}
 					else {
@@ -155,7 +171,7 @@ int main(int argc, char **argv)
 				}
 				break;
 			case meditate:
-				// TODO
+				// TODO // Hmm.. chyba gotowe , nie? Po co to TODO?
 				if(time(NULL)>=meditateEnd) {
 						mastersPower[myMaster].first=50;
 						mastersPower[myMaster].second++;
@@ -165,8 +181,11 @@ int main(int argc, char **argv)
 						msg[3]=mastersPower[myMaster].second;
 						myMaster=-1;
 						for(int i=0; i<size; i++)
-							if(i!=tid)
+							if(i!=tid) {
 								MPI_Send(&msg, 4, MPI_INT, i, RELEASE_MASTER_TAG, MPI_COMM_WORLD );
+								gettimeofday(&tv, NULL);
+								cout<<tv.tv_sec*1000000+tv.tv_usec<<" # "<<tid<<": send RELEASE_MASTER_TAG to: " << i << "  (power " <<msg[2]<<" v"<< msg[3]<< ")"<<endl;
+							}
 						stan=vacant;
 				}
 				break;
@@ -198,13 +217,8 @@ int main(int argc, char **argv)
 					gettimeofday(&tv, NULL);
 					cout<<tv.tv_sec*1000000+tv.tv_usec<<" # "<<tid<<": REQUEST from "<<status.MPI_SOURCE<<" about "<<msg4[0]<<" "<<msg4[1]<<endl;
 					if(msg4[0]==myRoom && stan==gettingRoom){
-						// odeślij znacznik czasowy
-						//jeżeli tamten lepszy, to sygnał wolny -1
-						//jeżeli tamten gorszy, to ingnore
-						//ew. tablice
-						//msg1 = lecturesDone;
-						//MPI_Send(&msg1, 1, MPI_INT, status.MPI_SOURCE, RESPONSE_TAG, MPI_COMM_WORLD );
-						//msg4[0]=-1;
+						//jeżeli tamten lepszy, to wyślij sygnał wolny
+						//jeżeli tamten gorszy, to ingnoruj
 						if(msg4[1]<lecturesDone || (msg4[1] == lecturesDone && status.MPI_SOURCE < tid) ) {
 							MPI_Send(&msg4, 4, MPI_INT, status.MPI_SOURCE, RESPONSE_TAG, MPI_COMM_WORLD );
 							gettimeofday(&tv, NULL);
@@ -212,8 +226,7 @@ int main(int argc, char **argv)
 						}
 					}
 					else if(msg4[0]!=myRoom){
-						// odeślij sygnał wolny -1
-						//msg4[0] = -1;
+						// odeślij sygnał wolny
 						MPI_Send(&msg4, 4, MPI_INT, status.MPI_SOURCE, RESPONSE_TAG, MPI_COMM_WORLD );
 						gettimeofday(&tv, NULL);
 						cout<<tv.tv_sec*1000000+tv.tv_usec<<" # "<<tid<<": Send response to "<<status.MPI_SOURCE<<endl;
@@ -246,15 +259,19 @@ int main(int argc, char **argv)
 					cout<<tv.tv_sec*1000000+tv.tv_usec<<" # "<<tid<<": RESPONSE"<<endl;
 				break;
 				case RELEASE_ROOM_TAG:
+					gettimeofday(&tv, NULL);
+					cout<<tv.tv_sec*1000000+tv.tv_usec<<" # "<<tid<<": RELEASE_ROOM_TAG from: " << status.MPI_SOURCE <<endl;
 					if(stan==gettingRoom && msg4[0]==myRoom)
 						responses.insert(status.MPI_SOURCE);
-					// usuwanie z kolejki:, może się przydać timestamp
+					// usuwanie z kolejki:
 					roomsQ[msg4[0]].erase(P(msg4[1],status.MPI_SOURCE));
 				break;
 				case RELEASE_MASTER_TAG:
+					gettimeofday(&tv, NULL);
+					cout<<tv.tv_sec*1000000+tv.tv_usec<<" # "<<tid<<": RELEASE_MASTER_TAG from: " << status.MPI_SOURCE << "  (power " <<msg[2]<<" v"<< msg[3]<< ")"<<endl;
 					if(stan==gettingMaster && msg4[0]==myMaster)
 						responses.insert(status.MPI_SOURCE);
-					// usuwanie z kolejki:, może się przydać timestamp
+					// usuwanie z kolejki:
 					mastersQ[msg4[0]].erase(P(msg4[1],status.MPI_SOURCE));
 					if(mastersPower[msg4[0]].second<msg4[3]) {
 						mastersPower[msg4[0]].first=msg4[2];
@@ -268,32 +285,6 @@ int main(int argc, char **argv)
 			flag=-1;
 
 		}
-			/*
-		
-		
-		
-		// ODBIOR RESPONSOW
-		MPI_Irecv(&msg1, 1, MPI_INT, MPI_ANY_SOURCE, RESPONSE_TAG, MPI_COMM_WORLD, &request);
-		MPI_Test(&request, &flag, &status);
-		
-		if(flag){
-			responses.insert(status.MPI_SOURCE);
-			cout<<tid<<": RESPONSE"<<endl;
-		}
-		
-		// ODBIOR RELEASOW
-		MPI_Irecv(msg5, 5, MPI_INT, MPI_ANY_SOURCE, RELEASE_TAG, MPI_COMM_WORLD, &request);
-		MPI_Test(&request, &flag, &status);
-		
-		if(flag){
-			if(msg5[0]==1) { //if room
-				if(stan==gettingRoom && msg5[1]==myRoom)
-					responses.insert(status.MPI_SOURCE);
-				// usuwanie z kolejki:, może się przydać timestamp
-				roomsQ[msg5[0]].erase(P(msg5[2],status.MPI_SOURCE));
-			}
-		}
-		*/
 		
 
 	}
@@ -303,18 +294,27 @@ int main(int argc, char **argv)
 }
 
 
-
+/*
 int getMaster(){
+	int min_size=100000, min_index=0;
+	for(int i=0;i<mastersNum;i++)
+		if(mastersQ[i].size()<min_size) {
+			min_size = mastersQ[i].size();
+			min_index = i;
+		}
 	
-	return -1;
+	return min_index;
 }
 
 
 int getRoom(){
-	/*
-	int myRoom = rand()%roomsNum;
-	roomsQ[myRoom].insert(P(lecturesDone, tid));
-	return myRoom;
-	*/
-	return -1;
+	int min_size=100000, min_index=0;
+	for(int i=0;i<roomsNum;i++)
+		if(roomsQ[i].size()<min_size) {
+			min_size = roomsQ[i].size();
+			min_index = i;
+		}
+	
+	return min_index;
 }
+*/
